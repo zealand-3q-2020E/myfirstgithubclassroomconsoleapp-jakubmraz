61c5299738f6d52045047f3dd07e4b33f9e72474
Jakub Mraz <jaku0899@edu.easj.dk>
2020-08-25 13:11:52 +0200

    Wrote some code

--- MyFirstGitHubClassRoomConsoleApp/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp/Program.cs
@@ -11,7 +11,25 @@ namespace MyFirstGitHubClassRoomConsoleApp
         static void Main(string[] args)
         {
             //Opgave tilføj en Console.writeline med noget tekst og commit + push
-            //se herefter på dit GitHub Classroom repo om ændringen er kommet op 
+            //se herefter på dit GitHub Classroom repo om ændringen er kommet op
+            if(1+1==2)
+                if(1+1==2)
+                    if (1 + 1 == 2)
+                    {
+                        Console.WriteLine("Cosnoom the chalice");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Are ya coding son");
+                    }
+                else
+                {
+                    Console.WriteLine("patreon link below");
+                }
+            else
+            {
+                Console.WriteLine("/ban chaliceconsumer1");
+            }
         }
     }
 }
